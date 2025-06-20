c8d7511731f2db5252048529676bb53622125619
Opanasenko-Mykhailo <124090722+Opanasenko-Mykhailo@users.noreply.github.com>
2025-06-20 13:03:15 +0300

    GCA-2 Init Spring Application - add spring mvc

--- src/main/java/com/gcs/app/GymApplication.java
+++ src/main/java/com/gcs/app/GymApplication.java
@@ -1,11 +1,6 @@
 package com.gcs.app;
 
-import com.gcs.app.config.AppConfig;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 public class GymApplication {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
     }
 }
