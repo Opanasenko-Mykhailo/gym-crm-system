37d38cfcd4bea084885305dd69e652d9cb80ddd0
Opanasenko-Mykhailo <124090722+Opanasenko-Mykhailo@users.noreply.github.com>
2025-07-08 12:41:25 +0300

    GCA-52 Update Gym services - Add validation for required fields

--- src/main/java/com/gcs/app/facade/constant/ValidationPatterns.java
+++ src/main/java/com/gcs/app/facade/constant/ValidationPatterns.java
@@ -1,9 +1,9 @@
 package com.gcs.app.facade.constant;
 
-public final class ValidationPatterns {
-    public static final String PASSWORD_PATTERN =
-            "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=[\\]{};':\"\\\\|,.<>/?]).{8,}$";
+import lombok.NoArgsConstructor;
+import lombok.AccessLevel;
 
-    private ValidationPatterns() {
-    }
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ValidationPatterns {
+    public static final String PASSWORD_PATTERN = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=[\\]{};':\"\\\\|,.<>/?]).{8,}$";
 }
