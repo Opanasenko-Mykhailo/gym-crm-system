33947a97466bf3bbb8dd8fb411f341c5db11921a
Opanasenko-Mykhailo <124090722+Opanasenko-Mykhailo@users.noreply.github.com>
2025-07-17 11:55:10 +0300

    GCA-68 Prepare Trainer Controller PoC

--- src/main/java/com/gcs/app/service/impl/TraineeServiceImpl.java
+++ src/main/java/com/gcs/app/service/impl/TraineeServiceImpl.java
@@ -143,8 +143,6 @@ public class TraineeServiceImpl implements TraineeService {
         return User.builder()
                 .username(username)
                 .password(password)
-                .firstName(user.getFirstName())
-                .lastName(user.getLastName())
                 .isActive(true)
                 .build();
     }
--- src/main/java/com/gcs/app/service/impl/TrainerServiceImpl.java
+++ src/main/java/com/gcs/app/service/impl/TrainerServiceImpl.java
@@ -109,8 +109,6 @@ public class TrainerServiceImpl implements TrainerService {
         return User.builder()
                 .username(username)
                 .password(password)
-                .firstName(user.getFirstName())
-                .lastName(user.getLastName())
                 .isActive(true)
                 .build();
     }
