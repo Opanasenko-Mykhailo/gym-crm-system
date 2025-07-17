23f69520f110b036769d0a8f2b38431ac72cf10a
Opanasenko-Mykhailo <124090722+Opanasenko-Mykhailo@users.noreply.github.com>
2025-07-17 16:51:01 +0300

    Revert "GCA-68 Prepare Trainer Controller PoC"
    This reverts commit 3a9d233450617ce160d207deee6debacce65c76a.

--- src/main/java/com/gcs/app/mapper/TrainerMapper.java
+++ src/main/java/com/gcs/app/mapper/TrainerMapper.java
@@ -4,16 +4,32 @@ import com.gcs.app.facade.dto.TrainerCreateRequestDto;
 import com.gcs.app.facade.dto.TrainerResponseDto;
 import com.gcs.app.facade.dto.TrainerUpdateRequestDto;
 import com.gcs.app.model.Trainer;
+import com.gcs.app.rest.TrainerRegistrationRequest;
+import com.gcs.app.rest.TrainerProfileResponse;
+import com.gcs.app.rest.TrainerUpdateRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-
 
 @Mapper(componentModel = "spring")
 public interface TrainerMapper {
 
+    @Mapping(target = "user.firstName", source = "firstName")
+    @Mapping(target = "user.lastName", source = "lastName")
     Trainer toEntity(TrainerCreateRequestDto dto);
 
     TrainerResponseDto toDto(Trainer trainer);
 
     Trainer update(@MappingTarget Trainer trainer, TrainerUpdateRequestDto dto);
+
+    TrainerCreateRequestDto toCreateRequestDto(TrainerRegistrationRequest request);
+
+    TrainerUpdateRequestDto toUpdateRequestDto(TrainerUpdateRequest request);
+
+    @Mapping(source = "user.firstName", target = "firstName")
+    @Mapping(source = "user.lastName", target = "lastName")
+    @Mapping(source = "user.username", target = "username")
+    @Mapping(source = "user.isActive", target = "isActive")
+    @Mapping(source = "specialization.name", target = "specialization")
+    TrainerProfileResponse toRestModel(Trainer trainer);
 }
