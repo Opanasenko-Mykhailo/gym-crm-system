21007023f0d17e9732ad7b43014af4f6969243b3
Opanasenko-Mykhailo <124090722+Opanasenko-Mykhailo@users.noreply.github.com>
2025-07-17 17:40:56 +0300

    GCA-68 Prepare Trainer Controller PoC

---
+++ src/main/java/com/gcs/app/controller/ApiConstant.java
@@ -0,0 +1,9 @@
+package com.gcs.app.controller;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ApiConstant {
+    public static final String BASE_PATH = "/api/v1";
+}
