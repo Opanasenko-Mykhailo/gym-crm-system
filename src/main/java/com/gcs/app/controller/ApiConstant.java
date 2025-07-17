3856c39f9ce23e1d6b8cd7a301e31869847778ba
Opanasenko-Mykhailo <124090722+Opanasenko-Mykhailo@users.noreply.github.com>
2025-07-17 17:22:48 +0300

    GCA-68 Refactor exist code

--- src/main/java/com/gcs/app/controller/ApiConstant.java
+++
@@ -1,9 +0,0 @@
-package com.gcs.app.controller;
-
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class ApiConstant {
-    public static final String BASE_PATH = "/api/v1";
-}
