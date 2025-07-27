7bbbf38ab77ac03a93a0a68b9e463fc1b44fac0a
Opanasenko-Mykhailo <124090722+Opanasenko-Mykhailo@users.noreply.github.com>
2025-07-27 23:15:41 +0300

    GCA-96 Cover aux endpoints with Swagger documentation

--- src/main/java/com/gcs/app/config/WebConfig.java
+++ src/main/java/com/gcs/app/config/WebConfig.java
@@ -1,27 +1,46 @@
 package com.gcs.app.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "com.gcs.app.controller")
+@ComponentScan(basePackages = {"com.gcs.app.controller", "org.springdoc"})
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        converter.setObjectMapper(objectMapper);
-        converters.add(converter);
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.stream()
+                .filter(MappingJackson2HttpMessageConverter.class::isInstance)
+                .map(MappingJackson2HttpMessageConverter.class::cast)
+                .forEach(converter -> converter.setObjectMapper(createCustomObjectMapper()));
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui.html**")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/v3/api-docs/**")
+                .addResourceLocations("classpath:/META-INF/resources/");
+    }
+
+    private ObjectMapper createCustomObjectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        return mapper;
     }
 }
--- src/main/java/com/gcs/app/controller/TrainingTypeController.java
+++ src/main/java/com/gcs/app/controller/TrainingTypeController.java
@@ -1,7 +1,15 @@
 package com.gcs.app.controller;
 
 import com.gcs.app.facade.GymFacade;
+import com.gcs.app.rest.ErrorResponse;
 import com.gcs.app.rest.TrainingTypeResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,14 +23,21 @@ import static com.gcs.app.controller.ApiConstant.BASE_PATH;
 @RestController
 @RequestMapping(BASE_PATH + "/training-types")
 @RequiredArgsConstructor
+@Tag(name = "Training", description = "Operations related to training sessions")
 public class TrainingTypeController {
 
     private final GymFacade facade;
 
+    @Operation(summary = "Get training types", description = "Retrieves the list of available training types")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of training types",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = TrainingTypeResponse.class)))),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @GetMapping
     public ResponseEntity<List<TrainingTypeResponse>> getTrainingTypes() {
         List<TrainingTypeResponse> trainingTypes = facade.getAllTrainingTypes();
-
         return ResponseEntity.ok(trainingTypes);
     }
 }
