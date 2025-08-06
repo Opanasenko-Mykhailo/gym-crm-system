9583f070171f6ff595de58a4066f22cfbb44863f
”Opanasenko-Mykhailo <opanasenko.misha@gmail.com>
2025-08-06 20:48:25 +0300

    GCA-132 Implement JWT-based authentication for login

--- src/test/java/com/gcs/app/security/JwtAuthFilterTest.java
+++ src/test/java/com/gcs/app/security/JwtAuthFilterTest.java
@@ -20,7 +20,6 @@ import java.util.Collections;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
